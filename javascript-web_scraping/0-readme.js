6fec01d422396428a45d8d822a6f8f642e483c0d
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-22 04:11:33 +0000

    README and Script that read and print file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* Script that reads and prints the content of a file */
+
+const fs = require('fs');
+const firstArg = process.argv[2];
+
+fs.readFile(firstArg,'utf-8',(err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data)
+});
