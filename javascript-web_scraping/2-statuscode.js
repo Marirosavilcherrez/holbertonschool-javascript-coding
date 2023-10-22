f3895eb4d5c71599dde5686666f1c8591d237486
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-22 05:40:02 +0000

    Script that display the status

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* Script that prints the title */
+
+const request = require('request');
+
+let getURL = process.argv[2];
+
+request(getURL, (error, response) => {  //configure the request
+  if (error) {
+    console.error(`Error`);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
