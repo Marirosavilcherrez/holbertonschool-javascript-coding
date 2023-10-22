b85c4cea20caef5f005d079aea328ddc3a857dc1
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-10-22 01:32:38 -0500

    Update 3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -6,7 +6,7 @@ const request = require('request');
 const movieID = process.argv[2];
 const URL = `https://swapi-api.hbtn.io/api/films/${movieID}`;
 
-request(URL, (error, response, body) => { //configure the request
+request(URL, (error, response, body) => { // configure the request
   if (!error && response.statusCode === 200) { // check status code
     const film = JSON.parse(body); // parse the response body as JSON
     console.log(film.title);
