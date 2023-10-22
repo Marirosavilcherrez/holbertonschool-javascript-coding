c288955eb257c8e071a36ed9c762993e5370c3b4
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-22 04:59:46 +0000

    Fix semistandard validation

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,10 +4,10 @@
 const fs = require('fs');
 const firstArg = process.argv[2];
 
-fs.readFile(firstArg,'utf-8',(err, data) => {
+fs.readFile(firstArg, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(data)
+  console.log(data);
 });
---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* Script that writes a string to a file */
+
+const fs = require('fs');
+const firstArg = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(firstArg, content,'utf-8',(err) => {
+  if (err) {
+  console.error(err);
+  return;
+  }
+});
