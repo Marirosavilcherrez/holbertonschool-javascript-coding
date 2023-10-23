1535e08b6790d5dfdf54e201c4dfbac94eee6497
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-23 03:28:56 +0000

    Script that get content of a web and store in file

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,16 +6,15 @@ const request = require('request');
 const apiUrl = process.argv[2];
 const characterId = 18;
 
-request(apiUrl, (error, response, body) => {  //configure the request
-  if (!error && response.statusCode === 200) { //check status code
-    const data = JSON.parse(body); //parse the response body as JSON
-    
+request(apiUrl, (error, response, body) => { //configure the request
+  if (!error && response.statusCode === 200) { // check status code
+    const data = JSON.parse(body); // parse the response body as JSON
     const movieName = data.results.filter((film) => // filter the movie
       film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)
     );
 
-    console.log(movieName.length)
+    console.log(movieName.length);
   } else {
-    console.error(`Error:`, error);
+    console.error('Error:', error);
   }
 });
