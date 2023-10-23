3e5b9f0bd3644976192d10f87169b354ac0cf061
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-10-22 22:30:12 -0500

    Update 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,7 @@ const request = require('request');
 const apiUrl = process.argv[2];
 const characterId = 18;
 
-request(apiUrl, (error, response, body) => { //configure the request
+request(apiUrl, (error, response, body) => { // configure the request
   if (!error && response.statusCode === 200) { // check status code
     const data = JSON.parse(body); // parse the response body as JSON
     const movieName = data.results.filter((film) => // filter the movie
