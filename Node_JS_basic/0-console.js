1273838091839eb2d89d67a730133f49bc1d01b8
Marirosa <arqmvilcherrez@gmail.com>
2023-11-04 17:27:43 -0500

    Fix lint errors

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /* Create a function that prints in STDOUT the string argument */
 
 function displayMessage(text) {
-    console.log(text);
-};
+  console.log(text);
+}
 
-module.exports = displayMessage; 
+module.exports = displayMessage;
