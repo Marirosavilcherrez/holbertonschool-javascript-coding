5482bfdd4b8009dc0d447e0f2201b44742372dea
Marirosa <arqmvilcherrez@gmail.com>
2023-11-05 18:56:02 -0500

    Fix error status

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -7,22 +7,19 @@ const port = 1245;
 
 const app = http.createServer((req, res) => {
   if(req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader = ('Content-Type', 'text/plain');
+    res.writeHead = (200, {'Content-Type': 'text/plain'});
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     const data = countStudents(process.argv[2]);
-    res.statusCode = 200;
-    res.setHeader = ('Content-Type', 'text/plain');
-    res.end(`This is the list of our students: ${data}\n`);
+    res.writeHead = (200, {'Content-Type': 'text/plain'});
+    res.end(`This is the list of our students: ${data}`);
   } else {
-    res.statusCode = 400;
-    res.setHeader = ('Content-Type', 'text/plain');
-    res.end('Not Found\n');
+    res.writeHead = (400, {'Content-Type': 'text/plain'});
+    res.end('Not Found');
   }
 });
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is runing on port ${port}`);
 });
 
 module.exports = app;
