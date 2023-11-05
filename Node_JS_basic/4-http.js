aa3bd13c0c99da4fe06db782102dd4e0648c2ffd
Marirosa <arqmvilcherrez@gmail.com>
2023-11-05 18:03:29 -0500

    Change listen port

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -4,11 +4,11 @@ const http = require('http');
 
 //create an HTTP server
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader = ('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!')
+  res.statusCode = 200;
+  res.setHeader = ('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!')
 });
 // Listen on port 1245
-server.listen(1245);
+app.listen(1245);
 
 module.exports = app;
