e4d46efe5c00267a534f1375eb4086c7f3f6618b
Marirosa <arqmvilcherrez@gmail.com>
2023-11-05 17:59:45 -0500

    Create a small HTTP server using Node's HTTP module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+/*  Create a small HTTP server using Node's HTTP module */
+
+const http = require('http');
+
+//create an HTTP server
+const app = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader = ('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!')
+});
+// Listen on port 1245
+server.listen(1245);
+
+module.exports = app;
