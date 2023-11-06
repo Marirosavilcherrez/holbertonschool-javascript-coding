f60979646fda6ee0a0a385744e150e97c82d04d1
Marirosa <arqmvilcherrez@gmail.com>
2023-11-05 19:19:52 -0500

    Fix lint errors

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 
 // Listen on port 1245
 app.listen(1245, () => {
-    console.log('The app works in http://localhost:1245')
+  console.log('The app works in http://localhost:1245');
 });
 
 module.exports = app;
